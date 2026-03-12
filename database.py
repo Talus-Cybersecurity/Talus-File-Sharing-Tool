e16b63849656fd65eeb49294f033fc84a2d784a7
mp28jam28 <mp28jam@csu.fullerton.edu>
2026-03-12 16:46:34 -0700

    TALUS-145 Set up database instance and initialize variables

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
+import sqlite3 as sql
 
+class Database:
+    def __init__(self): 
+        self.con = sql.connect("sample.db")
+        self.cur = self.con.cursor()
+
+    def run(self):
+        self.con.close()
+  
+if __name__ == "__main__":
+    storage = Database()
+    storage.run()
