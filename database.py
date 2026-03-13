d8307b4d7fb35fef351c5648753991c653058ff9
mp28jam28 <mp28jam@csu.fullerton.edu>
2026-03-12 23:38:17 -0700

    TALUS-147 Create User database table

--- database.py
+++ database.py
@@ -4,8 +4,18 @@ class Database:
     def __init__(self): 
         self.con = sql.connect("sample.db")
         self.cur = self.con.cursor()
-
+        
+    def create_user_table(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS User(
+                         user_id TEXT PRIMARY KEY,
+                         username TEXT NOT NULL UNIQUE,
+                         password TEXT NOT NULL,
+                         tag_id TEXT)
+                         """)  
+        
     def run(self):
+        self.create_user_table
+        self.con.commit()
         self.con.close()
   
 if __name__ == "__main__":
