2027527cc97bc074d51a6b82e73ac7cace3488cf
mp28jam28 <mp28jam@csu.fullerton.edu>
2026-03-17 23:34:51 -0700

    TALUS-150 Create File database table
    Updated table names to use PascalCase and added create_file_table method.

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ class Database:
 
     def create_user_table(self):
         self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS users(
+            CREATE TABLE IF NOT EXISTS "User"(
                 user_id TEXT PRIMARY KEY,
                 username TEXT NOT NULL UNIQUE,
                 password TEXT NOT NULL,
@@ -23,20 +23,37 @@ class Database:
 
     def create_accesslog_table(self):
         self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS accesslog (
+            CREATE TABLE IF NOT EXISTS "AccessLog" (
                 log_id TEXT PRIMARY KEY,
                 user_id TEXT,
                 file_id TEXT,
                 access_attempts INTEGER,
                 timestamps TIMESTAMP,
                 ip_address TEXT,
-                access_status TEXT
+                access_status TEXT,
+                FOREIGN KEY (user_id) REFERENCES "User"(user_id)
+            )
+        """)
+
+    def create_file_table(self):
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS "File" (
+                file_id TEXT PRIMARY KEY,
+                log_id TEXT,
+                owner_id TEXT,
+                file_type TEXT,
+                upload_timestamp TIMESTAMP,
+                file_size TEXT,
+                file_name TEXT,
+                FOREIGN KEY (log_id) REFERENCES AccessLog(log_id),
+                FOREIGN KEY (owner_id) REFERENCES "User"(user_id)
             )
         """)
 
     def run(self):
         self.create_user_table()
         self.create_accesslog_table()
+        self.create_file_table()
         self.con.commit()
         self.con.close()
 
